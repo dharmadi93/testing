b436e4aee26c9212277f2e7a240f6d6e3c4e3257
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-05 16:59:18 +0700

    check string more than four works

--- test/index.js
+++ test/index.js
@@ -3,7 +3,13 @@ const expect = require('chai').expect
 const lib = require('../lib/lib')
 
 describe("Test isEmpty", () => {
-    it("returns true if cater is empty", () => {
+    it("returns true if string is empty", () => {
         expect(lib.isEmpty([])).to.be.true
+    })
+})
+
+describe("Test MoreThanFour", () => {
+    it("returns true if string is more than four", () => {
+        expect(lib.moreThanFour("testing")).to.be.true
     })
 })
