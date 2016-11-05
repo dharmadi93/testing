64d89926d4d0d14c75ea7235da778ef3a5426c23
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-05 17:14:46 +0700

    is number works

--- test/index.js
+++ test/index.js
@@ -2,14 +2,23 @@ const mocha = require('mocha')
 const expect = require('chai').expect
 const lib = require('../lib/lib')
 
+// 1 IS EMPT
 describe("Test isEmpty", () => {
     it("returns true if string is empty", () => {
         expect(lib.isEmpty([])).to.be.true
     })
 })
 
+// 2 MROE THAN FOUR
 describe("Test MoreThanFour", () => {
     it("returns true if string is more than four", () => {
         expect(lib.moreThanFour("testing")).to.be.true
+    })
+})
+
+// IS NUMBER
+describe("Test isNumber", () => {
+    it("returns true if all numbers", () => {
+        expect(lib.isNumber("21312")).to.be.true
     })
 })
