438e8c1f890095bec7acddeb85a8bda3aa3ab82a
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-05 16:53:47 +0700

    testing is empy works

--- test/index.js
+++ test/index.js
@@ -1,3 +1,9 @@
 const mocha = require('mocha')
 const expect = require('chai').expect
 const lib = require('../lib/lib')
+
+describe("Test isEmpty", () => {
+    it("returns true if cater is empty", () => {
+        expect(lib.isEmpty([])).to.be.true
+    })
+})
